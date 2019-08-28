99e39fa1b448facdcd0a20aa8f8aad400690bc11
James Hong <dudgh00@me.com>
2019-08-28 03:26:56 +0000

    init

--- cmd.sh
+++ cmd.sh
@@ -5,11 +5,5 @@ dir_tomcat=/usr/local/tomcat/bin
 
 httpd-foreground &
 
-<<<<<<< HEAD
 ## run tomcat foreground !!
 $dir_tomcat/catalina.sh run
-=======
-$dir_tomcat/catalina.sh start
-
-/bin/bash
->>>>>>> 1b139f093a69f6b0f699fa41e5a265383c02e2d6
